0ce74ab8c17761e6f166d4678c37beb533ee5737
Iwá Duarte <iwaduarte@gmail.com>
2024-09-08 22:12:23 -0300

    feat: improve game logic when switching menus and add music

--- game/game.gd
+++ game/game.gd
@@ -1,17 +1,21 @@
 class_name Game extends Node2D
 
-@onready var main_menu: Main_Menu = $CanvasLayer/MainManu
+@onready var main_menu: Main_Menu = $CanvasLayer/MainMenu
 @onready var game_over: Game_Over = $CanvasLayer/GameOver
+@onready var audio_node = $CanvasLayer/MainMenu/BackgroundMusic
+	
 
 const GAME_BOARD_SCENE = preload("res://game_board/game_board.tscn")
 var game_board:Game_Board
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	game_over.hide()
 	main_menu.start_game.connect(_on_start_game)
 
 func _on_start_game() -> void:
 	main_menu.hide()
 	game_board = GAME_BOARD_SCENE.instantiate()
+	audio_node.stop()
 	add_child(game_board)
 	
 	game_board.game_finished.connect(_on_game_over)
@@ -25,6 +29,7 @@ func _on_game_over(score:int) -> void:
 	game_over.score(score)
 	
 func _on_back_pressed() -> void:
-	main_menu.show()
-	game_over.back.disconnect(_on_back_pressed)
 	game_over.hide()
+	game_over.back.disconnect(_on_back_pressed)
+	main_menu.show()
+	audio_node.play()
